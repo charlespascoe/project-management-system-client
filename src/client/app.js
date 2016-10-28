4844ec6c1939d5160937952fa1c68ca17f67f5ea
Charles Pascoe <cpascoe95@gmail.com>
2016-10-28 19:33:43 +0100

    Added notifications that appear at the bottom of the page

--- src/client/app.js
+++ src/client/app.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import router from 'client/navigation/router';
 import viewMap from 'client/navigation/view-map';
 import LoginView from 'client/views/login-view';
+import { SuccessNotification } from 'client/views/notifications';
 
 class App extends Component {
   constructor() {
     super();
-    this.state = {view: LoginView};
+    this.state = {view: LoginView,h:false};
     router.changeView = this.changeView.bind(this);
+    setTimeout(function () { this.setState({h:true}); }.bind(this), 2000);
   }
 
   changeView(viewId, navArgs) {
@@ -17,7 +19,10 @@ class App extends Component {
 
   render() {
     return (
-      <this.state.view navArgs={this.state.navArgs} />
+      <div>
+        <this.state.view navArgs={this.state.navArgs} />
+        <SuccessNotification message="test" hide={this.state.h} />
+      </div>
     );
   }
 }
---
+++ src/client/views/notifications.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import {
+  SuccessAlert,
+  InfoAlert,
+  WarningAlert,
+  DangerAlert
+} from 'client/views/alerts';
+
+class Notification extends Component {
+  render() {
+    return (
+      <div className={`notification ${this.props.hide ? ' hidden-notification' : ''}`}>
+        <this.alert message={this.props.message} />
+      </div>
+    );
+  }
+}
+
+export class SuccessNotification extends Notification {
+  constructor(props) {
+    super(props);
+    this.alert = SuccessAlert;
+  }
+}
+
+export class InfoNotification extends Notification {
+  constructor(props) {
+    super(props);
+    this.alert = InfoAlert;
+  }
+}
+
+export class WarningNotification extends Notification {
+  constructor(props) {
+    super(props);
+    this.alert = WarningAlert;
+  }
+}
+
+export class DangerNotification extends Notification {
+  constructor(props) {
+    super(props);
+    this.alert = DangerAlert;
+  }
+}
