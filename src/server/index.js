ea7a3ea3fd20785984abc6644d51e59342ad0faf
Charles Pascoe <cpascoe95@gmail.com>
2016-10-28 18:41:47 +0100

    Added logger and basic server

--- src/server/index.js
+++ src/server/index.js
@@ -1,1 +1,8 @@
-console.log('Server');
+import express from 'express';
+import logger from 'server/logger';
+
+const app = express();
+
+app.use('/public', express.static(__dirname + '/public'));
+
+app.listen(8080, (e) => e ? logger.fatal({err: e}) : logger.info('Listening on port 8080'));
