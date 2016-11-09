6b67713de45c1c4939b17b2f2732531c7a83295c
Charles Pascoe <cpascoe95@gmail.com>
2016-11-09 16:54:33 +0000

    Added HomeNavigator

---
+++ src/client/navigation/home-navigator.js
@@ -0,0 +1,21 @@
+import router from 'client/navigation/router';
+import authenticationClient from 'client/auth/authentication-client';
+import loginNavigator from 'client/navigation/login-navigator';
+
+export class HomeNavigator {
+  constructor(router, loginNavigator, authClient) {
+    this.router = router;
+    this.loginNavigator = loginNavigator;
+    this.authClient = authClient;
+  }
+
+  async startApp() {
+    if (this.authClient.initialise()) {
+      await this.router.navigate('home');
+    } else {
+      await this.loginNavigator.goToLogin();
+    }
+  }
+}
+
+export default new HomeNavigator(router, loginNavigator, authenticationClient);
