e3b4b1144ff6c388e6e33cdd74b2e64e5099d98e
Charles Pascoe <cpascoe95@gmail.com>
2016-11-09 16:46:22 +0000

    Updated app.js to use new navigation style

--- src/client/app.js
+++ src/client/app.js
@@ -1,27 +1,30 @@
+import 'client/views';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import router from 'client/navigation/router';
 import viewMap from 'client/navigation/view-map';
-import LoginView from 'client/views/login-view';
+import homeNavigator from 'client/navigation/home-navigator';
 import { SuccessNotification } from 'client/views/notifications';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {view: LoginView,h:false};
-    router.changeView = this.changeView.bind(this);
-    setTimeout(function () { this.setState({h:true}); }.bind(this), 2000);
+  componentDidMount() {
+    homeNavigator.startApp();
   }
 
-  changeView(viewId, navArgs) {
-    this.setState({view: viewMap.getView(viewId), navArgs: navArgs});
+  constructor() {
+    super();
+    this.state = {view: null};
+    router.changeView = this.changeView.bind(this);
+  }
+
+  changeView(nav) {
+    this.setState({view: viewMap.getView(nav.viewId), nav: nav});
   }
 
   render() {
     return (
       <div>
-        <this.state.view navArgs={this.state.navArgs} />
-        <SuccessNotification message="test" hide={this.state.h} />
+        {this.state.view ? <this.state.view nav={this.state.nav} /> : ''}
       </div>
     );
   }
