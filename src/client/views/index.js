cce4659db8116deb8e4b978940e176bb8df03ad2
Charles Pascoe <cpascoe95@gmail.com>
2016-11-09 16:54:52 +0000

    Added views index module, to register all the views to the ViewMap

---
+++ src/client/views/index.js
@@ -0,0 +1,7 @@
+import viewMap from 'client/navigation/view-map';
+
+import LoginView from 'client/views/login-view';
+viewMap.setView('login', LoginView);
+
+import HomeView from 'client/views/home-view';
+viewMap.setView('home', HomeView);
