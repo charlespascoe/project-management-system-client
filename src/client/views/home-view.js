9e9bcc380ee643994e8d53acee1550ae9dc345d6
Charles Pascoe <cpascoe95@gmail.com>
2016-11-09 16:58:09 +0000

    Fixed issue caused by HomeView not exporting itself as default

--- src/client/views/home-view.js
+++ src/client/views/home-view.js
@@ -2,7 +2,7 @@ import React from 'react';
 import View from 'client/views/view';
 import HomeViewmodel from 'client/viewmodels/home-viewmodel';
 
-export class HomeView extends View {
+export default class HomeView extends View {
   constructor(props) {
     super(props, HomeViewmodel.createDefault());
   }
