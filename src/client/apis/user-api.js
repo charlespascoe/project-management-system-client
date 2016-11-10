08f3c3b4ddc1d2fc488b3cfe0f8cfe5563288dc1
Charles Pascoe <cpascoe95@gmail.com>
2016-11-10 18:45:09 +0000

    Added UserApi

---
+++ src/client/apis/user-api.js
@@ -0,0 +1,29 @@
+import Response from 'client/apis/response';
+import User from 'client/models/user';
+import authenticationClient from 'client/auth/authentication-client';
+import {
+  defaultStatus
+} from 'client/apis/statuses';
+
+export class UserApi {
+  constructor(client) {
+    this.client = client;
+  }
+
+  async getUser(idOrEmail) {
+    return new Response(defaultStatus(200), new User({
+      firstName: 'Bob',
+      otherNames: 'Smith'
+    }));
+
+    var restResponse = await this.client.get(`/users/${encodeURIComponent(idOrEmail)}`);
+
+    var response = new Response(defaultStatus(restResponse.statusCode));
+
+    if (response.isOk) response.data = new User(restResponse.data);
+
+    return response;
+  }
+}
+
+export default new UserApi(authenticationClient);
