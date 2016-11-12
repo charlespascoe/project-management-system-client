7e7e8ef167372b3c12514af2ce6d458896e7a474
Charles Pascoe <cpascoe95@gmail.com>
2016-11-12 17:49:25 +0000

    Added UserManager.requestElevation and dropElevation

--- src/client/managers/user-manager.js
+++ src/client/managers/user-manager.js
@@ -1,5 +1,9 @@
 import authenticationClient from 'client/auth/authentication-client';
-import usersApi from 'client/apis/user-api';
+import usersApi from 'client/apis/users-api';
+import authApi from 'client/apis/authentication-api';
+import {
+  UnauthorisedStatus
+} from 'client/apis/statuses';
 
 export class UserManager {
   get user() { return this._user; }
@@ -8,9 +12,10 @@ export class UserManager {
     if (this.onUserChanged) this.onUserChanged(value);
   }
 
-  constructor(authClient, usersApi) {
+  constructor(authClient, usersApi, authApi) {
     this.authClient = authClient;
     this.usersApi = usersApi;
+    this.authApi = authApi;
     this.user = null;
   }
 
@@ -60,6 +65,36 @@ export class UserManager {
     this.user = null;
     await this.authClient.logout();
   }
+
+  async requestElevation(password) {
+    // Force a token refresh
+    var refreshResponse = await this.authClient.refresh();
+
+    if (!refreshResponse.isOk) return refreshResponse;
+
+    var response = await this.authApi.requestElevation(this.authClient.tokens.accessToken, password);
+
+    if (!response.isOk) return response;
+
+    this.user.sysadmin = true; // If the above request was successful, assume they are sysadmin
+    this.user.sysadminElevationExpires = response.data.sysadminElevationExpires;
+
+    // This is to cover any issues involving the expiry timestamp being in the past
+    if (!this.user.isSysadminElevated) return new Response(new UnauthorisedStatus());
+
+    if (this.onUserChanged) this.onUserChanged();
+
+    return response;
+  }
+
+  async dropElevation() {
+    var response = await this.authApi.dropElevation(this.authClient.tokens.accessToken);
+
+    // Unauthorised response (403) if the user isn't a sysadmin, which although it shouldn't happen, is fine
+    if (response.status instanceof UnauthorisedStatus) response.status = new SuccessStatus();
+
+    return response;
+  }
 }
 
-export default new UserManager(authenticationClient, usersApi);
+export default new UserManager(authenticationClient, usersApi, authApi);
