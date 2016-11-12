d3fd4c39f38fc7da87cac4b7662ed828da4ea239
Charles Pascoe <cpascoe95@gmail.com>
2016-11-12 17:47:49 +0000

    Added User.sysadmin and sysadminElevationExpires, as well as isSysadminElevated property

--- src/client/models/user.js
+++ src/client/models/user.js
@@ -1,8 +1,11 @@
 export default class User {
   get name() { return this.firstName + ' ' + this.otherNames; }
+  get isSysadminElevated() { return this.sysadmin && this.sysadminElevationExpires && this.sysadminElevationExpires > Date.now(); }
 
   constructor(data) {
-    this.firstName = data.firstName;
-    this.otherNames = data.otherNames;
+    this.firstName = data.firstName || '';
+    this.otherNames = data.otherNames || '';
+    this.sysadmin = data.sysadmin;
+    this.sysadminElevationExpires = data.sysadminElevationExpires ? new Date(data.sysadminElevationExpires) : null;
   }
 }
