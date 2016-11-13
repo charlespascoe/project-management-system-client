9fff326161a574038d749b0b85fbe766aea8bc1b
Charles Pascoe <cpascoe95@gmail.com>
2016-11-13 18:28:22 +0000

    Refactored HomeNavigator to improve expired token handling

--- src/client/navigation/home-navigator.js
+++ src/client/navigation/home-navigator.js
@@ -1,26 +1,34 @@
 import router from 'client/navigation/router';
 import userManager from 'client/managers/user-manager';
 import loginNavigator from 'client/navigation/login-navigator';
+import dialogueManager from 'client/managers/dialogue-manager';
 
 export class HomeNavigator {
-  constructor(router, loginNavigator, userManager) {
+  constructor(router, loginNavigator, userManager, dialogueManager) {
     this.router = router;
     this.loginNavigator = loginNavigator;
     this.userManager = userManager;
+    this.dialogueManager = dialogueManager;
+    this.userManager.onLogout = this.onLogout.bind(this);
+  }
+
+  async requestElevationDialogue() {
+    this.dialogueManager.showRequestElevationDialogue();
   }
 
   async startApp() {
     if (await this.userManager.initialise()) {
       await this.router.navigate('home');
-    } else {
-      await this.loginNavigator.goToLogin();
     }
   }
 
   async logout() {
-    await this.loginNavigator.goToLogin();
     await this.userManager.logout();
+  }
+
+  async onLogout() {
+    await this.loginNavigator.goToLogin();
   }
 }
 
-export default new HomeNavigator(router, loginNavigator, userManager);
+export default new HomeNavigator(router, loginNavigator, userManager, dialogueManager);
