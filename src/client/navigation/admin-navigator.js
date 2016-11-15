c960d4d7a8adbb0472be6dc136e3089de970a0d7
Charles Pascoe <cpascoe95@gmail.com>
2016-11-15 18:34:25 +0000

    Added AdminNavigator class

---
+++ src/client/navigation/admin-navigator.js
@@ -0,0 +1,22 @@
+import router from 'client/navigation/router';
+import AddUserDialogue from 'client/views/dialogues/admin/add-user-dialogue';
+import dialogueManager from 'client/managers/dialogue-manager';
+
+class AdminNavigator {
+  constructor(router, dialogueManager) {
+    this.router = router;
+    this.dialogueManager = dialogueManager;
+
+    this.router.view('admin').onDoneGoTo('home');
+  }
+
+  showAddUserDialogue() {
+    this.dialogueManager.showDialogue(AddUserDialogue);
+  }
+
+  async goToAdmin() {
+    this.router.navigate('admin');
+  }
+}
+
+export default new AdminNavigator(router, dialogueManager);
