e8f18f20c1d128b8575dcf576842ab6ab7cc766e
Charles Pascoe <cpascoe95@gmail.com>
2016-11-17 13:49:05 +0000

    Added Utils.updateArray

--- src/client/utils.js
+++ src/client/utils.js
@@ -16,4 +16,43 @@ export default class Utils {
   static textToBase64(text) {
     return btoa(text);
   }
+
+  static updateArray(sourceArray, destArray, keyComparator, replaceAction = null) {
+    var sourcePos = 0,
+        destPos = 0;
+
+    while (destPos < destArray.length) {
+      if (sourcePos == sourceArray.length) {
+        // No more items in source list - remove remaining items
+        destArray.splice(destPos, destArray.length - destPos);
+        break;
+      }
+
+      var comarasonResult = keyComparator(sourceArray[sourcePos], destArray[destPos]);
+
+      if (comarasonResult < 0) {
+        // New item - insert into list
+        destArray.splice(destPos, 0, sourceArray[sourcePos]);
+        destPos++;
+        sourcePos++;
+      } else if (comarasonResult == 0) {
+        // Same item - replace
+        if (replaceAction) {
+          destArray[destPos] = replaceAction(sourceArray[sourcePos], destArray[destPos]);
+        }
+
+        destPos++;
+        sourcePos++;
+      } else if (comarasonResult > 0 ) {
+        // Item removed - remove item at position
+        destArray.pop(destPos);
+      }
+    }
+
+    // Adds additional items on the end
+    while (sourcePos < sourceArray.length) {
+      destArray.push(sourceArray[sourcePos]);
+      sourcePos++;
+    }
+  }
 }
