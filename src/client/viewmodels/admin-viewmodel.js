46edcfcb2283d1574133bec50c67a6dc3a1f10cc
Charles Pascoe <cpascoe95@gmail.com>
2016-11-17 13:54:46 +0000

    Users can now be deleted from Admin view

--- src/client/viewmodels/admin-viewmodel.js
+++ src/client/viewmodels/admin-viewmodel.js
@@ -1,20 +1,60 @@
 import Viewmodel from 'client/viewmodels/viewmodel';
-import usersManager from 'client/managers/users-manager';
 import userManager from 'client/managers/user-manager';
+import usersManager from 'client/managers/users-manager';
 import adminNavigator from 'client/navigation/admin-navigator';
+import notificationQueue from 'client/notification-queue';
+import Utils from 'client/utils';
 import {
   UnauthorisedStatus,
   UnauthenticatedStatus
 } from 'client/apis/statuses';
 
 class UserViewmodel extends Viewmodel {
-  get id() { return this.user.id; }
+  get id() { return this.model.id; }
 
-  get fullName() { return this.user.name; }
+  get isCurrentUser() { return this.id == this.userManager.user.id }
 
-  constructor(user) {
+  get fullName() { return this.model.name; }
+
+  get model() { return this._model; }
+  set model(value) { this._model = value; this.changed(); }
+
+  get loading() { return this._loading; }
+  set loading(value) { this._loading = value; this.changed(); }
+
+  constructor(user, adminNavigator, userManager, usersManager, notificationQueue, onDelete) {
     super();
-    this.user = user;
+    this.model = user;
+    this.adminNavigator = adminNavigator;
+    this.userManager = userManager;
+    this.usersManager = usersManager;
+    this.notificationQueue = notificationQueue;
+    this.onDelete = onDelete;
+  }
+
+  static createDefault(user, onDelete) {
+    return new UserViewmodel(user, adminNavigator, userManager, usersManager, notificationQueue, onDelete);
+  }
+
+  async delete() {
+    if (this.loading) return;
+
+    var confirmDelete = await this.adminNavigator.showConfirmDeleteUser(this.fullName);
+
+    if (!confirmDelete) return;
+
+    this.loading = true;
+    var response = await this.usersManager.deleteUser(this.id);
+    this.loading = false;
+
+    if (response.isOk) {
+      this.notificationQueue.showSuccessNotification(`Removed ${this.fullName} as a user`);
+    } else if (!(response.status instanceof UnauthenticatedStatus || response.status instanceof UnauthorisedStatus)) {
+      this.notificationQueue.showDangerNotification(`Something went wrong when trying to delete ${this.fullName}`);
+      return;
+    }
+
+    this.onDelete();
   }
 }
 
@@ -28,22 +68,21 @@ export default class AdminViewmodel extends Viewmodel {
   get loading() { return this._loading; }
   set loading(value) { this._loading = value; this.changed(); }
 
-  constructor(userManager, usersManager, adminNavigator) {
+  constructor(usersManager, adminNavigator, notificationQueue) {
     super();
-    this.userManager = userManager;
     this.usersManager = usersManager;
     this.adminNavigator = adminNavigator;
+    this.notificationQueue = notificationQueue;
     this.users = [];
     this.projects = [];
   }
 
   static createDefault() {
-    return new AdminViewmodel(userManager, usersManager, adminNavigator);
+    return new AdminViewmodel(usersManager, adminNavigator, notificationQueue);
   }
 
   onEnter(nav) {
     super.onEnter(nav);
-
     this.refresh();
   }
 
@@ -59,20 +98,17 @@ export default class AdminViewmodel extends Viewmodel {
 
     var usersResponse = await this.usersManager.getAllUsers();
 
-    if (!usersResponse.isOk) {
-      if (usersResponse.status instanceof UnauthorisedStatus) {
-        this.userManager.elevationExpired();
-        return;
-      } else if (usersResponse.status instanceof UnauthenticatedStatus) {
-        // Unauthenticated responses automatically go to login page
-        return;
-      }
+    // Unauthenticated and Unauthorised responses already handled by UsersManager
+    if (usersResponse.status instanceof UnauthorisedStatus || usersResponse.status instanceof UnauthenticatedStatus) return;
 
-      // Something went wrong!
+    if (!usersResponse.isOk) {
+      this.notificationQueue.showDangerNotification('Something when wrong when contacting the server');
+      this.loading = false;
       return;
     }
 
-    this.users = usersResponse.data.map(user => new UserViewmodel(user));
+    Utils.updateArray(usersResponse.data.map(user => UserViewmodel.createDefault(user, () => this.refresh())), this.users, (user1, user2) => user1.id - user2.id, (newVm, oldVm) => oldVm.model = newVm.model);
+    this.changed();
 
     this.loading = false;
   }
