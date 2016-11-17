f3af9b781377aa5f9f6bff940bf7a6312c505407
Charles Pascoe <cpascoe95@gmail.com>
2016-11-17 13:48:28 +0000

    Utils now adds a pop method to the Array prototype

--- src/client/utils.js
+++ src/client/utils.js
@@ -1,3 +1,9 @@
+Array.prototype.pop = function (index = null) {
+  if (this.length == 0) return;
+  if (index == null) index = this.length - 1;
+  return this.splice(index, 1)[0];
+};
+
 export default class Utils {
   static defaults(defaults, additional) {
     return Object.assign(Utils.copy(defaults), additional);
