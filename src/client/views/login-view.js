852f9af08edd914038c1cb01f0c803fd49ba8fe8
Charles Pascoe <cpascoe95@gmail.com>
2016-11-19 19:40:07 +0000

    Login view style now matches the rest of the application

--- src/client/views/login-view.js
+++ src/client/views/login-view.js
@@ -23,54 +23,55 @@ export default class LoginView extends View {
 
   render() {
     return (
-      <div className='container'>
-        <header>
+      <div>
+        <div className='container-flex header-bar'>
           <h1 className='text-center'>Login</h1>
-        </header>
-
-        <form className='form-signin' id='signin' onSubmit={this.onSubmit.bind(this)}>
-          <label htmlFor='inputUsername' className='sr-only'>Username</label>
-          <input
-            type='text'
-            id='inputUsername'
-            className='form-control'
-            placeholder='Username'
-            required
-            autoFocus
-            autoComplete='off'
-            value={this.viewmodel.username}
-            onChange={bindValue(this.viewmodel, 'username')}
-            disabled={this.viewmodel.loading}
-          />
-
-          <label htmlFor='inputPassword' className='sr-only'>Password</label>
-          <input
-            type='password'
-            id='inputPassword'
-            className='form-control'
-            placeholder='Password'
-            required
-            autoComplete='off'
-            value={this.viewmodel.password}
-            onChange={bindValue(this.viewmodel, 'password')}
-            disabled={this.viewmodel.loading}
-          />
-
-          <div id='remember-me-container'>
+        </div>
+        <div className='container'>
+          <form className='form-signin' id='signin' onSubmit={this.onSubmit.bind(this)}>
+            <label htmlFor='inputUsername' className='sr-only'>Username</label>
             <input
-              type='checkbox'
-              id='remember-me'
-              checked={this.viewmodel.rememberMe}
-              onChange={bindChecked(this.viewmodel, 'rememberMe')}
+              type='text'
+              id='inputUsername'
+              className='form-control'
+              placeholder='Username'
+              required
+              autoFocus
+              autoComplete='off'
+              value={this.viewmodel.username}
+              onChange={bindValue(this.viewmodel, 'username')}
               disabled={this.viewmodel.loading}
             />
-            <label htmlFor='remember-me'>Remember Me</label>
-          </div>
 
-          <PrimaryButton text='Log in' disabled={!this.viewmodel.isValid || this.viewmodel.loading} onClick={this.login.bind(this)} />
-          {this.viewmodel.loading ? <LoadingAlert message='Signing in...' /> : ''}
-          {this.viewmodel.errorMessage ? <DangerAlert message={this.viewmodel.errorMessage} /> : ''}
-        </form>
+            <label htmlFor='inputPassword' className='sr-only'>Password</label>
+            <input
+              type='password'
+              id='inputPassword'
+              className='form-control'
+              placeholder='Password'
+              required
+              autoComplete='off'
+              value={this.viewmodel.password}
+              onChange={bindValue(this.viewmodel, 'password')}
+              disabled={this.viewmodel.loading}
+            />
+
+            <div id='remember-me-container'>
+              <input
+                type='checkbox'
+                id='remember-me'
+                checked={this.viewmodel.rememberMe}
+                onChange={bindChecked(this.viewmodel, 'rememberMe')}
+                disabled={this.viewmodel.loading}
+              />
+              <label htmlFor='remember-me'>Remember Me</label>
+            </div>
+
+            <PrimaryButton text='Log in' disabled={!this.viewmodel.isValid || this.viewmodel.loading} onClick={this.login.bind(this)} />
+            {this.viewmodel.loading ? <LoadingAlert message='Signing in...' /> : ''}
+            {this.viewmodel.errorMessage ? <DangerAlert message={this.viewmodel.errorMessage} /> : ''}
+          </form>
+        </div>
       </div>
     );
   }
