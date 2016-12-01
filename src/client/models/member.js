0d08fbe48d964f1dbff87b21ce9529dc53cba727
Charles Pascoe <cpascoe95@gmail.com>
2016-12-01 10:48:13 +0000

    Added start of Member model

---
+++ src/client/models/member.js
@@ -0,0 +1,28 @@
+import membersApi from 'client/apis/members-api';
+
+export default class Member {
+  get name() { return this.user.firstName + ' ' + this.user.otherNames; }
+
+  constructor(data, membersApi) {
+    this.user = data.user;
+    this.role = data.role;
+    this.project = data.project;
+    this.membersApi = membersApi;
+  }
+
+  static createDefault(data) {
+    return new Member(data, membersApi);
+  }
+
+  async updateMemberRole(roleId) {
+    var response = await this.membersApi.updateMemberRole(this.project.id, this.user.id, roleId);
+
+    if (response.isOk) this.role = {id: roleId};
+
+    return response;
+  }
+
+  async remove() {
+    return await this.membersApi.removeMember(this.project.id, this.user.id);
+  }
+}
