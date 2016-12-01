12ed057695e6b0ec293972d0d59bf0c4bed0d12a
Charles Pascoe <cpascoe95@gmail.com>
2016-12-01 10:46:38 +0000

    Added Response.isUnauthorised and isUnauthenticated for convenience

--- src/client/apis/response.js
+++ src/client/apis/response.js
@@ -1,7 +1,15 @@
-import { SuccessStatus } from 'client/apis/statuses';
+import {
+  SuccessStatus,
+  UnauthenticatedStatus,
+  UnauthorisedStatus
+} from 'client/apis/statuses';
 
 export default class Response {
   get isOk() { return this.status instanceof SuccessStatus }
+
+  get isUnauthenticated() { return this.status instanceof UnauthenticatedStatus; }
+
+  get isUnauthorised() { return this.status instanceof UnauthorisedStatus; }
 
   constructor(status, data) {
     this.status = status;
