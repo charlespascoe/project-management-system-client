e78be87a9e0b27785af19c2e61c8bf14709ff689
Charles Pascoe <cpascoe95@gmail.com>
2016-12-06 08:51:59 +0000

    Added Response.noInternet property

--- src/client/apis/response.js
+++ src/client/apis/response.js
@@ -1,7 +1,8 @@
 import {
   SuccessStatus,
   UnauthenticatedStatus,
-  UnauthorisedStatus
+  UnauthorisedStatus,
+  NoInternetStatus
 } from 'client/apis/statuses';
 
 export default class Response {
@@ -10,6 +11,8 @@ export default class Response {
   get isUnauthenticated() { return this.status instanceof UnauthenticatedStatus; }
 
   get isUnauthorised() { return this.status instanceof UnauthorisedStatus; }
+
+  get noInternet() { return this.status instanceof NoInternetStatus; }
 
   constructor(status, data) {
     this.status = status;
