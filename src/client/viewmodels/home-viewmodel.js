82143645d1dc21e421f90c26c7a52198b4fe50bc
Charles Pascoe <cpascoe95@gmail.com>
2016-12-07 13:54:37 +0000

    Added list of assignments on Home view

--- src/client/viewmodels/home-viewmodel.js
+++ src/client/viewmodels/home-viewmodel.js
@@ -1,13 +1,73 @@
 import Viewmodel from 'client/viewmodels/viewmodel';
+import userManager from 'client/managers/user-manager';
+import notificationQueue from 'client/notification-queue';
+import projectNavigator from 'client/navigation/project-navigator';
+
+class AssignmentViewmodel {
+  get id() { return this.model.project.id; }
+  get projectName() { return this.model.project.name; }
+  get roleName() { return this.model.role.name; }
+
+  constructor(member, projectNavigator) {
+    this.model = member;
+    this.projectNavigator = projectNavigator;
+    console.log(this.model);
+  }
+
+  static create(member) {
+    return new AssignmentViewmodel(member, projectNavigator);
+  }
+
+  async goToProject() {
+    await this.projectNavigator.viewProject(this.model.project);
+  }
+}
 
 export default class HomeViewmodel extends Viewmodel {
+  get assignments() { return this._assignments; }
+  set assignments(value) { this._assignments = value; this.changed(); }
+
+  get loadingAssignments() { return this._loadingAssignments; }
+  set loadingAssignments(value) { this._loadingAssignments = value; this.changed(); }
+
+  constructor(userManager, notificationQueue) {
+    super();
+    this.assignments = [];
+    this.userManager = userManager;
+    this.loadingAssignments = false;
+    this.notificationQueue = notificationQueue;
+  }
+
+  static createDefault() {
+    return new HomeViewmodel(userManager, notificationQueue);
+  }
+
   onEnter(nav) {
     super.onEnter(nav);
     this.nav.clearHistory();
     this.nav.viewmodel = this;
+    this.loadAssignments();
   }
 
-  static createDefault() {
-    return new HomeViewmodel();
+  async loadAssignments() {
+    if (this.loadingAssignments) return;
+
+    this.loadingAssignments = true;
+    var response = await this.userManager.user.getProjectAssignments();
+    this.loadingAssignments = false;
+
+    if (response.isOk) {
+      this.assignments = response.data.map(member => AssignmentViewmodel.create(member));
+      return;
+    }
+
+    if (response.isUnauthenticated || response.isUnauthorised) return;
+
+    if (response.noInternet) {
+      this.notificationQueue.showDangerNotification('No Internet Connection');
+      return;
+    }
+
+    this.notificationQueue.showDangerNotification('Something went wrong when contacting the server');
   }
 }
--- src/client/views/home-view.js
+++ src/client/views/home-view.js
@@ -1,6 +1,22 @@
 import React from 'react';
 import View from 'client/views/view';
 import HomeViewmodel from 'client/viewmodels/home-viewmodel';
+import CollapsiblePanel from 'client/views/collapsible-panel';
+import Icon from 'client/views/icon';
+import { LoadingAlert, AlertContainer } from 'client/views/alerts';
+
+function AssignmentView(props) {
+  return (
+    <tr>
+      <td>{props.viewmodel.id}</td>
+      <td>{props.viewmodel.projectName}</td>
+      <td>{props.viewmodel.roleName}</td>
+      <td className='view-project button' onClick={() => props.viewmodel.goToProject()}>
+        <Icon type='chevron-right absolute-center' />
+      </td>
+    </tr>
+  );
+}
 
 export default class HomeView extends View {
   constructor(props) {
@@ -9,9 +25,35 @@ export default class HomeView extends View {
 
   render() {
     return (
-      <div>
+      <div id='home-view'>
         <div className='container-flex header-bar'>
           <h1 className='text-center'>Home</h1>
+        </div>
+        <div className='container'>
+          <AlertContainer collapsed={!this.viewmodel.loadingAssignments}>
+            <LoadingAlert message='Loading...' />
+          </AlertContainer>
+          <CollapsiblePanel title='My Projects' type='primary' padding={false}>
+            <table className='table table-striped no-margin' id='user-projects-table'>
+              <colgroup>
+                <col id='project-id-col' />
+                <col id='project-name-col' />
+                <col id='role-col' />
+                <col id='view-project-col' />
+              </colgroup>
+              <thead>
+                <tr>
+                  <th>Project ID</th>
+                  <th>Project Name</th>
+                  <th>Role</th>
+                  <th>View</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.viewmodel.assignments.map(assignmentVm => <AssignmentView viewmodel={assignmentVm} key={assignmentVm.id} />)}
+              </tbody>
+            </table>
+          </CollapsiblePanel>
         </div>
       </div>
     );
