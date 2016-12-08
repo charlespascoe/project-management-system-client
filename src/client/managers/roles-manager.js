f581ab1614463af9b07c202a15baad8bd9a6e982
Charles Pascoe <cpascoe95@gmail.com>
2016-12-08 10:25:45 +0000

    RolesManager now keeps a reference to roles

--- src/client/managers/roles-manager.js
+++ src/client/managers/roles-manager.js
@@ -3,11 +3,16 @@ import rolesApi from 'client/apis/roles-api';
 export class RolesManager {
   constructor(rolesApi) {
     this.rolesApi = rolesApi;
+    this.roles = null;
   }
 
-  async getRoles() {
-    // Saving roles to local storage would be done here etc.
-    return await this.rolesApi.getRoles();
+  // Saving roles to local storage would be done here etc.
+  async getRoles(forceRefresh = false) {
+    var response = await this.rolesApi.getRoles();
+
+    if (response.isOk) this.roles = response.data;
+
+    return response;
   }
 }
 
